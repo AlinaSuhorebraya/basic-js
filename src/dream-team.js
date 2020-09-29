319dc39db0a4563e7341b3071753a54732fd4fbe
Alina_SUhorebraya <suchalina80@gmail.com>
2020-09-29 22:03:12 +0300

    feat: add solution in what-season.js file

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,28 +2,18 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   let newArr = [];
-
-//   members.forEach(member =>{
-//  if(typeof member === String)
-//     {
-//      member = member.split(' ').join('');
-//       newArr.push(member[0]);
-//     }
-//     return newArr.sort();
-
-//   })
+  
 if(members){
-for(let i = 0; i < members.length; i++)
-{
-  if( typeof members[i]=='string')
-    {
- 
-     members[i] = members[i].split(' ').join('');
-      newArr.push(members[i][0].toUpperCase());
-    }
-    
-}
-return newArr.sort().toString().split(',').join('');
-}
+  for(let i = 0; i < members.length; i++)
+  {
+    if( typeof members[i] == 'string') {
+  
+      members[i] = members[i].split(' ').join('');
+        newArr.push(members[i][0].toUpperCase());
+      }
+      
+  }
+  return newArr.sort().toString().split(',').join('');
+  }
 else return false
 };
