c0e1a401c794dc7e4b91386e24a8b83b84ddde6c
Alina_SUhorebraya <suchalina80@gmail.com>
2020-09-30 21:19:56 +0300

    feat: add solution to hanoi-tower task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( /*arr*/ ) {
+//   let newArr = [];
+//   if(arr)
+//   {
+    
+//  arr.forEach((a, index)=>{
+//    newArr.push(a);
+//    if(a == '--discard-next')
+//    {
+//      newArr.splice(0, index+1);
+//    }
+//    if(a == '--discard-prev')
+//    {
+//      newArr.splice(0, index);
+//    }
+
+//  })
+//  return newArr;
+// }
+// else return new Error();
+throw new CustomError('Not implemented');
 };
