40e041e84cf41e0011e3e2fe5759d75237ece7e3
Alina_SUhorebraya <suchalina80@gmail.com>
2020-09-30 22:36:53 +0300

    feat: add solution to transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,24 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform( /*arr*/ ) {
-//   let newArr = [];
-//   if(arr)
-//   {
-    
-//  arr.forEach((a, index)=>{
-//    newArr.push(a);
-//    if(a == '--discard-next')
-//    {
-//      newArr.splice(0, index+1);
-//    }
-//    if(a == '--discard-prev')
-//    {
-//      newArr.splice(0, index);
-//    }
+module.exports = function transform( arr ) {
+  
+  let newArr = arr;
+  if(!Array.isArray(arr))
+  {
+    throw new Error();
+  }
 
-//  })
+  if(arr)
+  {
+
+newArr.forEach((a, i)=>
+  {
+   if(newArr[i] == '--discard-next')
+   {
+     
+     newArr[i] = undefined;
+     if(i+1<newArr.length){
+     newArr[i+1]=undefined;
+     }
+   }
+   else if(newArr[i] == '--discard-prev')
+   {
+     newArr[i] = undefined;
+     if(i-1>0){
+    newArr[i-1]=undefined;
+     }
+   }
+   else if(newArr[i] == '--double-next')
+   {
+    if(i+1<newArr.length){
+    newArr[i] = newArr[i+1];
+    }
+    else newArr[i]=undefined;
+    
+   }
+   else if(newArr[i] == '--double-prev')
+   {
+    if(i-1>0){
+    newArr[i] = newArr[i-1];
+    }
+    else newArr[i]=undefined;
+   }
+
+ })
+
+//  newArr = newArr.filter((e)=>e!=undefined);
 //  return newArr;
-// }
-// else return new Error();
-throw new CustomError('Not implemented');
+for(let i = 0; i <newArr.length; i++)
+{
+  if(newArr[i]==undefined)
+  {
+    newArr.splice(i,0);
+    i--;
+  }
+}
+return newArr;
+}
+else return new Error();
+
 };
