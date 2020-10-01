6c109bb188ab0f32f08303ad74791a88e9d63878
Alina_SUhorebraya <suchalina80@gmail.com>
2020-10-01 10:08:19 +0300

    feat: change solution in transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,62 +2,66 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
   
-  let newArr = arr;
+  let newArr = arr.slice(0);
   if(!Array.isArray(arr))
   {
     throw new Error();
   }
 
-  if(arr)
-  {
+ 
 
-newArr.forEach((a, i)=>
+    for (let i = 0; i < arr.length; i++) 
   {
-   if(newArr[i] == '--discard-next')
-   {
+    switch(arr[i])
+    {
+   case '--discard-next':
+   
      
      newArr[i] = undefined;
      if(i+1<newArr.length){
      newArr[i+1]=undefined;
-     }
+     
    }
-   else if(newArr[i] == '--discard-prev')
-   {
+   break;
+   case '--discard-prev':
+   
      newArr[i] = undefined;
      if(i-1>0){
     newArr[i-1]=undefined;
      }
-   }
-   else if(newArr[i] == '--double-next')
-   {
+   break;
+  case'--double-next':
+   
     if(i+1<newArr.length){
     newArr[i] = newArr[i+1];
     }
-    else newArr[i]=undefined;
+    else {newArr[i]=undefined};
     
-   }
-   else if(newArr[i] == '--double-prev')
-   {
-    if(i-1>0){
+   break;
+ case '--double-prev':
+   
+    if(i-1 > 0){
     newArr[i] = newArr[i-1];
     }
-    else newArr[i]=undefined;
-   }
-
- })
+    else {newArr[i]=undefined};
+  break;
+  } 
+ }
 
 //  newArr = newArr.filter((e)=>e!=undefined);
 //  return newArr;
 for(let i = 0; i <newArr.length; i++)
 {
-  if(newArr[i]==undefined)
+  if(newArr[i]===undefined)
   {
-    newArr.splice(i,0);
-    i--;
+    newArr.splice(i,1);
+  i--;
   }
+  
 }
 return newArr;
-}
-else return new Error();
+
+  
+
 
 };
