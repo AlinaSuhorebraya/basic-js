c11735a4e53bf725a3c3103e13f4baa0a64cc006
Alina_SUhorebraya <suchalina80@gmail.com>
2020-10-04 22:09:53 +0300

    feat: add solution to simple-chain.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -5,4 +5,31 @@ module.exports = class DepthCalculator {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
+//   constructor ()
+//   {
+//     this.deep=0;
+//   }
+
+//   calculateDepth( arr ) {
+//     if(arr){
+  
+//  Array.from(arr).forEach((e,i)=>{
+
+//    if (Array.isArray(e))
+//    {
+//    {
+   
+//     this.calculateDepth(e);
+//     this.deep++;
+ 
+
+    
+//    }
+//  }
+
+
+// })
+// return this.deep;
+//   }
+// }
 };
